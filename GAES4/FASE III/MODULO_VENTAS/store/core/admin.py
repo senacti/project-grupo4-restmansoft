be8e0a77d5af52a1d4c0d12d3490058d2b50500e
demorales51 <110184945+demorales51@users.noreply.github.com>
2022-08-29 18:51:24 -0500

    Delete GAES4/FASE III/MODULO_VENTAS/store/core directory

--- GAES4/FASE III/MODULO_VENTAS/store/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from.models import Category,Product,Brand
-
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Brand)
